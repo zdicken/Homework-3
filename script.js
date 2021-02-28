9e3ad52e94546dffb4bc7b3c6ac0b541b0666ad9
zdicken <78879218+zdicken@users.noreply.github.com>
2021-02-28 15:48:02 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 var animals = ["dog", "fish", "cat", "shark", "cat", "dog"];
 var text = document.getElementById("yeah");
+read();
 
-for(var i = 0; i < animals.length; i++) {
-  if(animals[i] == "dog") {
-       text.innerHTML += "borf borf<br>";
-  } else if(animals[i] == "cat") {
-       text.innerHTML += "I am a cat<br>";
-  } else {
-       text.innerHTML += "I'm an animal<br>";
+function read() {
+  text.innerHTML = "";
+  for(var i = 0; i < animals.length; i++) {
+    if(animals[i] == "dog") {
+        text.innerHTML += "borf borf<br>";
+    } else if(animals[i] == "cat") {
+        text.innerHTML += "I am a cat<br>";
+    } else {
+        text.innerHTML += "I'm an animal<br>";
+    }
   }
+}
+
+function shuffle() {
+  animals.sort(() => Math.random() - 0.5);
+  read();
 }
